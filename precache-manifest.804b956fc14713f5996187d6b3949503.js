85c680438ec6f710b3bc74583fcf9fd553f2a192
Matt Wood <matt9663@gmail.com>
2020-04-24 15:10:55 -0400

    Updates

---
+++ precache-manifest.804b956fc14713f5996187d6b3949503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e4269d898216dc79c8161e6dd921c6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b56879ea7ea9c09eb7a",
+    "url": "/monsters-rolodex/static/css/main.23e31d67.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b56879ea7ea9c09eb7a",
+    "url": "/monsters-rolodex/static/js/main.b99ea2a7.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
